ef9a23b56852aa5352814b56771ff421385bb776
Kaleb Lopes Almeida <138385830+kalebalmeida@users.noreply.github.com>
2024-12-19 20:52:37 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let input_domain = document.getElementById('domain')
 let span = document.getElementById('success-span')
 let submtd_form = document.querySelector('.submitted-form')
+let botao_ok = document.querySelector('.ok-button')
 
 let form1Enviado = false;
     
@@ -27,16 +28,19 @@ function domainSubmited(e){
     input_domain.style.border = '2px solid #2cc76a'
     span.style.display = 'block'
 }
+form1.addEventListener("submit", domainSubmited)
 
 function tiraInteracao(){
     input_domain.style.border = 'none'
     span.style.display = 'none'
     form1Enviado = false
 }
+input_domain.addEventListener("input", tiraInteracao)
 
 function tiraSubmittedForm(){
     submtd_form.style.display = 'none'
 }
+botao_ok.addEventListener("click", tiraSubmittedForm)
 
 
 
